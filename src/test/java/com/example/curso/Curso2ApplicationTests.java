8cede0bcdd1e9618f30b5c26aa8083a98a481c6f
pauloricardolongi <pauloricardolongi@gmail.com>
2021-04-24 06:21:43 -0300

    Projeto criado

---
+++ src/test/java/com/example/curso/Curso2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Curso2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
